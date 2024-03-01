60b837a7fb2c09efef5f58bb4c8bc5d99475c742
Samuel Gómez <samuelgomez0618@hotmail.com>
2024-03-01 16:35:25 -0400

    assest location modified

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -18,31 +18,31 @@ export class CardsComponent {
   TAGS = {
     React: {
       name: 'React.js',
-      icon: '../../assets/icons/react-javascript-js.svg',
+      icon: 'assets/icons/react-javascript-js.svg',
     },
     Tailwind: {
       name: 'Tailwind CSS',
-      icon: '../../assets/icons/tailwind-css-svgrepo-com.svg',
+      icon: 'assets/icons/tailwind-css-svgrepo-com.svg',
     },
     Node: {
       name: 'Express.js',
-      icon: '../../assets/icons/node-js.svg',
+      icon: 'assets/icons/node-js.svg',
     },
     Postgres: {
       name: 'Postgresql',
-      icon: '../../assets/icons/PostgreSQL-Logo.wine.svg',
+      icon: 'assets/icons/PostgreSQL-Logo.wine.svg',
     },
     Sequelize: {
       name: 'Sequelize',
-      icon: '../../assets/icons/sequelize-svgrepo-com.svg',
+      icon: 'assets/icons/sequelize-svgrepo-com.svg',
     },
     Angular: {
       name: 'Angular',
-      icon: '../../assets/icons/angular-icon-seeklogo.svg',
+      icon: 'assets/icons/angular-icon-seeklogo.svg',
     },
     Daisyui: {
       name: 'Daisyui',
-      icon: '../../assets/icons/daisyui.svg',
+      icon: 'assets/icons/daisyui.svg',
     },
   };
   projects = [
@@ -51,8 +51,8 @@ export class CardsComponent {
       title: 'Samuel Gomez Portfolio',
       description: 'This is my personal portfolio. For this one I use Angular with Taildwin CSS and Daisyui for fast style development. It also use SSR and is fully mobile responsive',
       year: '2024',
-      bImage: '../../assets/portfolio.png',
-      wImage: '../../assets/portfolio-w.png',
+      bImage: 'assets/portfolio.png',
+      wImage: 'assets/portfolio-w.png',
       tags: [this.TAGS.Tailwind, this.TAGS.Angular, this.TAGS.Daisyui],
       github: 'https://github.com/XamS9/xams-portfolio',
       mockupLink: 'portfolio.com',
@@ -63,8 +63,8 @@ export class CardsComponent {
       description: `It's web platform based on udemy, you can learn about anything with online courses. As an admin you can create, read, update and delete every data with the dashboard and also subscribe to any course and view the content like a regular student as well.
       `,
       year: '2023',
-      bImage: '../../assets/bootcamp.png',
-      wImage: '../../assets/bootcamp-w.png',
+      bImage: 'assets/bootcamp.png',
+      wImage: 'assets/bootcamp-w.png',
       tags: [
         this.TAGS.React,
         this.TAGS.Node,
